5b08337ecb7f31b7c8d642f6a1c89b4157698315
tanmayrane18 <tanmayrane51@gmail.com>
2023-08-06 15:20:07 +0530

    modal card created

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert("Yee buddy light weight")
+const modal = document.querySelector(".modal")
+const overlay = document.querySelector(".overlay")
+
+//Modal open function
+const openModal = () => {
+    console.log("Modal is open")
+    modal.classList.add("active")
+    overlay.classList.add("overlayactive")
+}
+
+//Modal close function
+const closeModal = () => {
+    console.log("Modal is close")
+    modal.classList.remove("active")
+    overlay.classList.remove("overlayactive")
+}
